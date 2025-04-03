0012489bcda77588ad4f58289e0fcb9346e97d74
sawa-zen <sawadasuiren@gmail.com>
2025-04-03 23:24:32 +0900

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! first

--- client/src/App/components/AvatarPanel/components/ThreeLayer/index.tsx
+++ client/src/App/components/AvatarPanel/components/ThreeLayer/index.tsx
@@ -1,9 +1,33 @@
 import { useFrame } from "@react-three/fiber"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import { GLTF, GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { VRMLoaderPlugin } from '@pixiv/three-vrm';
+import { Stats, OrbitControls } from '@react-three/drei'
+
 import type { Mesh } from "three"
 
 export const ThreeLayer = () => {
+  const [gltf, setGltf] = useState<GLTF | null>(null)
   const cubeRef = useRef<Mesh>(null)
+
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    loader.register((parser) => {
+      return new VRMLoaderPlugin(parser);
+    });
+    loader.load(
+      // URL of the VRM you want to load
+      '/sample.vrm',
+      // called when the resource is loaded
+      (gltf) => {
+        setGltf(gltf)
+      },
+      // called while loading is progressing
+      (progress) => console.log('Loading model...', 100.0 * (progress.loaded / progress.total), '%'),
+      // called when loading has errors
+      (error) => console.error(error),
+    );
+  }, [])
 
   useFrame(() => {
     if (cubeRef.current) {
@@ -15,13 +39,14 @@ export const ThreeLayer = () => {
 
   return (
     <>
+      <OrbitControls />
+      <Stats />
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <mesh ref={cubeRef} rotation={[1, 0, 1]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="red" />
-      </mesh>
+      {gltf ? (
+        <primitive object={gltf.scene} position={[0, -1, 0]} dispose={null} />
+      ) : null}
     </>
   )
 }
