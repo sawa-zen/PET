b71d73ac4d56411490e9ebd710518079d26a9113
sawa-zen <sawadasuiren@gmail.com>
2025-04-03 23:31:55 +0900

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! first

--- client/src/App/components/AvatarPanel/components/ThreeLayer/index.tsx
+++ client/src/App/components/AvatarPanel/components/ThreeLayer/index.tsx
@@ -1,8 +1,8 @@
-import { useFrame } from "@react-three/fiber"
+import { useFrame, useThree } from "@react-three/fiber"
 import { useEffect, useRef, useState } from "react"
 import { GLTF, GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import { VRMLoaderPlugin } from '@pixiv/three-vrm';
-import { Stats, OrbitControls } from '@react-three/drei'
+import { OrbitControls, Stats } from '@react-three/drei'
 
 import type { Mesh } from "three"
 
@@ -29,6 +29,12 @@ export const ThreeLayer = () => {
     );
   }, [])
 
+  useThree(({ camera }) => {
+    camera.position.set(0, 0.25, 0.8)
+    camera.lookAt(0, 0.25, 0)
+    camera.updateProjectionMatrix()
+  })
+
   useFrame(() => {
     if (cubeRef.current) {
       cubeRef.current.rotation.x += 0.01
@@ -39,13 +45,15 @@ export const ThreeLayer = () => {
 
   return (
     <>
-      <OrbitControls />
       <Stats />
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
       {gltf ? (
-        <primitive object={gltf.scene} position={[0, -1, 0]} dispose={null} />
+        <primitive
+          object={gltf.scene}
+          position={[0, -1, 0]}
+          dispose={null}
+        />
       ) : null}
     </>
   )
